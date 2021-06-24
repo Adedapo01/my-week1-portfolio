84f0345b9f438db9635286d6b5b688b390da92fd
Adedapo <adedapo.adeola20@gmail.com>
2021-06-24 19:53:14 +0000

    Implementing JSON Walkthrough

--- portfolio/src/main/webapp/NewScript.js
+++ portfolio/src/main/webapp/NewScript.js
@@ -1,8 +1,21 @@
 /** Fetches the current date from the server and adds it to the page. */
-async function addRandomGreeting() {
-  const greetings = await fetch('/hello');
-  const greeting = await greetings.text();
+async function addRandomQuote() {
+    const responseFromServer = await fetch('/hello');
+    const quotes = await responseFromServer.json();
+    
+    const quoteContainer = document.getElementById('quote-container');
+    
+        const randNum = Math.floor((Math.random()* 3) +1);
+        if (randNum == 1){
+            quoteContainer.innerText = 'Quote 1: Live And Let Live';
+        }
+        else if (randNum == 2){
+            quoteContainer.innerText = 'Quote 2: All Fingers Are Not Equal';
+            
+        }
+        else if (randNum == 3){
+            quoteContainer.innerText = 'Quote 3: Another Man\'s Food Is Another Man\'s Poison';
+        }
 
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+
 }
